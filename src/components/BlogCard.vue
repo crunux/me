296f1ece51e651ab4e17f105b89aa52586861363
CrossDev <joancruz0502@hotmail.com>
2023-12-02 21:32:11 -0400

    chore: show blog

---
+++ src/components/BlogCard.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { Blog } from "~/types";
+interface Props {
+  blog: Blog;
+}
+
+const {blog} = defineProps<Props>();
+
+console.log(blog);
+</script>
+<template>
+  <div class="">
+    <img :src="blog.image.url" :alt="blog.tittle" />
+    <h1>{{ blog.tittle }}</h1>
+    <p>{{ blog.content }}</p>
+    <span>{{ blog.createdAt }}</span>
+    <RouterLink :to="`/blog/${blog.slug}`"></RouterLink>
+  </div>
+</template>
+<style scoped></style>
--- src/pages/blog/index.vue
+++ src/pages/blog/index.vue
@@ -1,22 +1,35 @@
 <script setup lang="ts">
+import type { Blog } from "~/types";
+type Data = {
+  posts: Blog[];
+};
 const query = gql`
-query GetPosts {
-  posts {
-    createdAt
-    id
-    slug
-    tittle
-    content
-    image {
-      url(
-        transformation: {document: {output: {format: webp}}, image: {resize: {fit: scale}}}
-      )
+  query getPosts {
+    posts {
+      createdAt
+      id
+      slug
+      tittle
+      content
+      image {
+        url(
+          transformation: {
+            document: { output: { format: webp } }
+            image: { resize: { width: 300, height: 300 } }
+          }
+        )
+      }
     }
   }
-}`
-const { data, error, refresh } = useAsyncQuery(query)
+`;
+
+const { data, error, refresh } = await useAsyncQuery<Data>(query);
+
+console.log(data);
 </script>
 <template>
-  <div class="app">{{data}}</div>
+  <section class="flex flex-row">
+    <BlogCard v-for="blog in data.posts" :key="blog.id" :blog="blog" />
+  </section>
 </template>
 <style scoped></style>
