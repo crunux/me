81d29077655feb8136360049d85166bc2f230576
CrossDev <joancruz0502@hotmail.com>
2023-12-03 15:11:55 -0400

    feat: layout for blog

---
+++ src/layouts/blog.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type { Route } from "~/types";
+const links = ref<Route[]>([
+  {
+    name: "Home",
+    page: "/",
+  },
+  {
+    name: "About",
+    page: "/about",
+  },
+  {
+    name: "Projects",
+    page: "/projects",
+  },
+  {
+    name: "Blog",
+    page: "/blog",
+  },
+  {
+    name: "Contact",
+    page: "/contact",
+  },
+]);
+</script>
+<template>
+  <NavBar activeRoute :links="links" />
+</template>
+<style scoped></style>
