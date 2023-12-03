ff348deaa6b8c0a97ff00d9c1907ef89e53a4e6d
CrossDev <joancruz0502@hotmail.com>
2023-12-03 15:13:41 -0400

    chore: add type and changer names

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,34 @@
-export interface Blog {
+export interface Posts {
     createdAt: string;
     id: string;
     slug: string;
     tittle: string;
+    image: Image;
+}
+
+export interface Post {
+    id: string;
+    tittle: string;
     content: string;
-    image: {
-        url: string;
-    }
+    slug: string;
+    image: Image;
+    createdAt: string;
+    createBy: CreatedBy
+
+}
+
+interface Image {
+    url: string;
+}
+interface CreatedBy {
+    name: string;
+    picture: string;
+    id: string;
+}
+
+export interface Route {
+
+    name: string;
+    page: string,
+
 }
