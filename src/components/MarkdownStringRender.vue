0417f9017bee8a21e3ace66a9d4e4c80f3471e85
CrossDev <joancruz0502@hotmail.com>
2023-12-03 20:29:27 -0400

    feat: create component to markdownString

---
+++ src/components/MarkdownStringRender.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const colorMode = useColorMode();
+interface Props {
+  tag: string;
+  markdownString: string;
+}
+
+const props = defineProps<Props>();
+</script>
+<template>
+  <div v-html="$mdRenderer.render(props.markdownString)"></div>
+</template>
+<style scoped></style>
