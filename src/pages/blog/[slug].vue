a2d134c10d344261ecf2d9825104c33df0e3872b
CrossDev <joancruz0502@hotmail.com>
2023-12-03 20:31:02 -0400

    feat: render markdown content

--- src/pages/blog/[slug].vue
+++ src/pages/blog/[slug].vue
@@ -5,6 +5,7 @@ type Data = {
 };
 const route = useRoute();
 const slug = route.params.slug;
+const mdcVars = ref({ name: "Maxime" });
 useHead({
   title: "Blog",
 });
@@ -49,16 +50,20 @@ const { data } = await useAsyncQuery<Data>(query, {
       class="grid place-items-center h-auto rounded-[20px] w-[90%] bg-[#c5c5c5] dark:bg-[#2d2e2e]"
     >
       <h1
-        class="font-ubuntu font-semibold m-3 p-3 text-[#2d2e2e] dark:text-[#d9d9d9]"
+        class="font-ubuntu font-semibold m-3 p-3 text-4xl text-[#2d2e2e] dark:text-[#d9d9d9]"
       >
         {{ data.post.tittle }}
       </h1>
       <div class="grid h-auto place-items-center">
-        <img :src="data.post.image.url" alt="" />
+        <img class="rounded-[20px]" :src="data.post.image.url" alt="" />
       </div>
 
       <div>
-        {{ data.post.content }}
+        <MarkdownStringRender
+          class="text-lg mt-3 text-[#2d2e2e] dark:text-[#d9d9d9]"
+          :markdownString="data.post.content"
+          :tag="data.post.id"
+        />
       </div>
     </div>
   </section>
