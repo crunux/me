72b4a23320f7b374b8690216f87f2d0537470613
CrossDev <joancruz0502@hotmail.com>
2023-12-10 08:51:27 -0400

    refactor: style

--- src/components/DarkSwitcher.vue
+++ src/components/DarkSwitcher.vue
@@ -3,11 +3,15 @@ const isDark = useDark();
 const toggleDark = useToggle(isDark);
 </script>
 <template>
-  <button
-    class="laptop:w-[32px] movil:w-[22px]"
-    dark:i-carbon-asleep-filled
-    i-carbon-awake
-    @click="toggleDark()"
-  ></button>
+  <div
+    class="flex justify-center items-center laptop:w-[36px] laptop:h-[35px] movil:w-[32px] movil:h-[32px] z-[100] bg-transparent rounded-full border-[#2d2e2e] dark:border-[#d9d9d9] border"
+  >
+    <button
+      class="laptop:text-[32px] movil:text-[22px] dark:text-[#d9d9d9] text-[#2d2e2e]"
+      dark:i-carbon-asleep-filled
+      i-carbon-awake
+      @click="toggleDark()"
+    ></button>
+  </div>
 </template>
 <style scoped></style>
