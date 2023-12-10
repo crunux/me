963f1bc97684f10df26e52c62a0a345f3d8b199e
CrossDev <joancruz0502@hotmail.com>
2023-12-10 11:43:17 -0400

    feat: add time since created

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type { Posts } from "~/types";
+import { timeSince } from '~/shared/helpers/time-since'
 interface Props {
   post: Posts;
 }
@@ -7,20 +8,26 @@ interface Props {
 const props = defineProps<Props>();
 </script>
 <template>
-  <NuxtLink class="cursor-pointer" :to="`/blog/${props.post.slug}`">
-    <div
-      class="flex flex-row justify-betweet items-center p-5 m-5 w-[60%] h-[250px] rounded-[20px] bg-[#c5c5c5] dark:bg-[#2d2e2e]"
-    >
-      <div>
+  <NuxtLink
+    class="cursor-pointer p-2 m-5 w-[80%] flex justify-center items-center rounded-[20px] bg-[#c5c5c5] dark:bg-[#2d2e2e]"
+    :to="`/blog/${props.post.slug}`"
+  >
+    <div class="flex flex-row justify-center items-center w-[80%] h-[250px]">
+      <div class="w-auto p-2 m-2">
         <img
-          class="rounded-[20px] h-60"
+          class="w-80 rounded-[20px]"
           :src="props.post.image.url"
           :alt="props.post.tittle"
         />
       </div>
-      <div flex="~ col" class="justify-betweet items-center">
-        <h1>{{ props.post.tittle }}</h1>
-        <span class="text">{{ props.post.createdAt }}</span>
+      <div
+        flex="~ col 1"
+        class="items-center text-[#2d2e2e] dark:text-[#d9d9d9]"
+      >
+        <h1 class="font-ubuntu text-[30px] font-bold">
+          {{ props.post.tittle }}
+        </h1>
+        <span class="opacity-50">{{ timeSince(props.post.createdAt) }} ago</span>
       </div>
     </div>
   </NuxtLink>
