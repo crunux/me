facb7b93e6aeb472d08a9196b83e7b9f56a211fe
CrossDev <joancruz0502@hotmail.com>
2023-12-10 07:26:11 -0400

    refactor: button toggle Dark

---
+++ src/components/DarkSwitcher.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+</script>
+<template>
+  <button
+    class="laptop:w-[32px] movil:w-[22px]"
+    dark:i-carbon-asleep-filled
+    i-carbon-awake
+    @click="toggleDark()"
+  ></button>
+</template>
+<style scoped></style>
