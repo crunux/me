686c8727904c03cf751cb80ff30f26bd05cf25ae
CrossDev <joancruz0502@hotmail.com>
2023-12-13 18:29:21 -0400

    refact: add shortcuts style

--- uno.config.ts
+++ uno.config.ts
@@ -6,5 +6,11 @@ export default defineConfig({
         presetAttributify(),
         presetIcons(),
         presetTypography(),
-    ]
+    ],
+    shortcuts: {
+        'text-color': 'text-[#2d2e2e] dark:text-[#d9d9d9]',
+        'borde-color': 'border-[#2d2e2e] dark:border-[#d9d9d9]',
+        'card-bg-color': 'bg-[#dfdede] dark:bg-[#4a4b4b]',
+        'section-bg-color': 'bg-[#c5c5c5] dark:bg-[#2d2e2e]',
+    },
 })
