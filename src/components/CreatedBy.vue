170a676dd1b56dc7a35a4f1cbebf378f15d6b8f5
crunux <joancruz0502@hotmail.com>
2024-07-25 23:00:56 -0400

    refactor(create by): add optional autor

--- src/components/CreatedBy.vue
+++ src/components/CreatedBy.vue
@@ -1,19 +1,13 @@
 <script setup lang="ts">
-import type { CreatedBy } from "~/types";
-interface Props {
-  createdBy: CreatedBy;
-}
-const { createdBy } = defineProps<Props>();
+  import type { CreatedBy } from "~/types";
+  interface Props {
+    createdBy?: CreatedBy;
+  }
+  defineProps<Props>();
 </script>
 <template>
-  <div
-    class="flex flex-row p-1 m-1 justify-center items-center gap2"
-  >
-    <img
-      class="rounded-full max-w-50px"
-      :src="createdBy.picture"
-      :alt="createdBy.id"
-    />
+  <div v-if="createdBy" class="flex flex-row p-1 m-1 justify-center items-center gap2">
+    <img class="rounded-full max-w-50px" :src="createdBy.picture" :alt="createdBy.id" />
     <h4 class="font-nunito font-semibold">
       {{ createdBy.name }}
     </h4>
