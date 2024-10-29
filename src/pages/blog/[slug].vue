26dcb0ede15dcad82f517b830ac8a6a39ecfd20b
crunux <joancruz0502@hotmail.com>
2024-10-29 16:19:00 -0400

    chore: change style

--- src/pages/blog/[slug].vue
+++ src/pages/blog/[slug].vue
@@ -54,8 +54,7 @@ console.log(data.value?.post.content)
 const { data: ast } = await useAsyncData('markdown', () => parseMarkdown(data.value?.post.content ?? ''))
 </script>
 <template>
-  <section
-    class="mt-20 px-4 pb-20 table:px-10 laptop:mx-auto laptop:max-w-[calc(100vw-5rem)] laptop:max-w-340 laptop:pb-28">
+  <section class="mt-20 px-4 pb-20 table:px-10 laptop:mx-auto laptop:max-w-[calc(100vw-5rem)] laptop:pb-28">
     <div class="mx-auto max-w-200 pt-10 table:pt-18">
       <img class="rounded-[20px]" :src="data?.post.image.url" :alt="data?.post.tittle" />
     </div>
@@ -66,10 +65,11 @@ const { data: ast } = await useAsyncData('markdown', () => parseMarkdown(data.va
         class="laptop:text-xl movil:text-sm table:text-lg text-start font-nunito w-[60%] mt-3 p-4 text-[#2d2e2e] dark:text-[#d9d9d9]"
         :markdownString="data?.post.content" :tag="data?.post.id" /> -->
     <MDCRenderer v-if="ast"
-      class="prose mt-10 w-full max-w-none overflow-hidden laptop:prose-xl prose-a:text-sky-500 prose-a:no-underline prose-pre:overflow-auto movil:text-xs table:text-lg laptop:text-xl text-start font-nunito text-[#2d2e2e] dark:text-[#d9d9d9]"
+      class="prose mt-10 laptop:w-[calc(100vw-30rem)] mx-auto max-w-none overflow-hidden laptop:prose-xl prose-a:text-sky-500 prose-a:no-underline prose-pre:overflow-auto movil:text-xs table:text-lg laptop:text-xl text-start font-nunito text-[#2d2e2e] dark:text-[#d9d9d9]"
       :body="ast.body" :data="ast.data" />
-    <div class="bottom-5 right-50 flex flex-col m-2 text-center text-[#2d2e2e] dark:text-[#d9d9d9]">
-      <h3 class="font-nunito font-semibold">Created By</h3>
+    <div
+      class="bottom-5 right-50 flex flex-col m-2 text-center text-sm laptop:text-xl text-[#2d2e2e] dark:text-[#d9d9d9]">
+      <h3 class="font-nunito font-semibold">Created By:</h3>
       <CreatedBy :createdBy="data?.post.createdBy" />
     </div>
   </section>
