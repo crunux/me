8cf6aaeea5f025f78a038d585f176072e30285d9
crunux <joancruz0502@hotmail.com>
2026-03-17 17:11:54 -0400

    chore: add bar time-read place above image

--- app/pages/blogs/[slug].vue
+++ app/pages/blogs/[slug].vue
@@ -16,26 +16,26 @@
 
 
 	const query = gql`
-		query getPost($locale: [Locale!]!, $slug: String!) {
-			post(locales: $locale, where: { slug: $slug }) {
-				id
-				createdAt
-				content
-				title
-				excerpt
-				readTime
-				tags
-				image {
-					url(transformation: { document: { output: { format: png } } })
-				}
-				createdBy {
-					name
-					picture
+			query getPost($locale: [Locale!]!, $slug: String!) {
+				post(locales: $locale, where: { slug: $slug }) {
 					id
+					createdAt
+					content
+					title
+					excerpt
+					readTime
+					tags
+					image {
+						url(transformation: { document: { output: { format: png } } })
+					}
+					createdBy {
+						name
+						picture
+						id
+					}
 				}
 			}
-		}
-	`;
+		`;
 
 	const { data, error } = await useAsyncQuery<Data>(query, {
 		locale: [locale.value],
@@ -54,33 +54,12 @@
 		router.push({ name: 'blogs' });
 	}
 
-	// useHead({
-	// 	title: data.value?.post.tittle,
-	// 	meta: [
-	// 		{
-	// 			name: "description",
-	// 			content: data.value?.post.excerpt,
-	// 		},
-	// 		{
-	// 			name: "og:title",
-	// 			content: `Crunux - ${data.value?.post.tittle}`,
-	// 		},
-	// 		{
-	// 			name: "og:description",
-	// 			content: data.value?.post.excerpt,
-	// 		},
-	// 		{
-	// 			name: "og:image",
-	// 			content: data.value?.post.image?.url,
-	// 		},
-	// 	],
-	// });
 	useSeoMeta({
-	  title: data.value?.post.title,
-	  ogTitle: `Crunux - ${data.value?.post.title}`,
-	  description: data.value?.post.slug,
-	  ogDescription: data.value?.post.slug,
-	  ogImage: data.value?.post.image,
+		title: data.value?.post.title,
+		ogTitle: `Crunux - ${data.value?.post.title}`,
+		description: data.value?.post.slug,
+		ogDescription: data.value?.post.slug,
+		ogImage: data.value?.post.image,
 	});
 
 	const { data: ast } = await useAsyncData('markdown', () =>
@@ -101,17 +80,22 @@
 
 		<article>
 			<header class="mb-8">
-				<div class="mx-auto max-w-200 mb-2 pt-10 table:pt-18">
-					<img class="rounded-md" :src="data?.post.image.url" :alt="data?.post.title" />
-				</div>
-				<!-- <h1 class="text-3xl font-bold tracking-tight text-foreground md:text-4xl text-pretty">
-          {{ ast?.data?.title }}
-        </h1> -->
-				<div class="mt-4 flex items-center gap-3 text-sm text-muted-foreground">
+				<h1 class="text-5xl font-nunito">{{ data?.post.title }}</h1>
+				<div class="mt-4 flex items-center gap-3 text-sm text-muted-foreground border-y p-2">
 					<time>ago {{ timeSince(data?.post.createdAt as string) }}</time>
 					<span class="h-1 w-1 rounded-full bg-border" />
 					<span>{{ data?.post.readTime }}</span>
 				</div>
+				<div class="mx-auto max-w-200 mb-2 pt-10 table:pt-18">
+					<img 
+						class="rounded-md" 
+						:src="data?.post.image.url" 
+						:alt="data?.post.title" 
+					/>
+				</div>
+				<!-- <h1 class="text-3xl font-bold tracking-tight text-foreground md:text-4xl text-pretty">
+          {{ ast?.data?.title }}
+        </h1> -->
 			</header>
 
 			<div class="prose prose-invert w-full">
@@ -119,11 +103,9 @@
 					{{ data?.post.excerpt }}
 				</p>
 				<div class="mt-8 rounded-lg px-2">
-					<MDCRenderer 
-						v-if="ast"
+					<MDCRenderer v-if="ast"
 						class="prose mt-10 laptop:w-[calc(100vw-20rem)] mx-auto max-w-none overflow-hidden laptop:prose-xl prose-a:text-sky-500 prose-a:no-underline prose-pre:overflow-auto movil:text-xs table:text-lg laptop:text-xl text-start font-nunito text-[#2d2e2e] dark:text-[#d9d9d9]"
-						:body="ast.body" 
-						:data="ast.data" />
+						:body="ast.body" :data="ast.data" />
 				</div>
 			</div>
 		</article>
